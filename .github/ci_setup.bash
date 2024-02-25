e7949e1e4f8f9d3a6ea51dd7887c64bfb46a7c20
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-02-25 12:31:02 -0800

    Add Xacro tooling and CI

---
+++ .github/ci_setup.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+
+set -euxo pipefail
+
+# At the moment, we don't need any setup actions for CI.
+# This file is a placeholder in case we ever do need any.
